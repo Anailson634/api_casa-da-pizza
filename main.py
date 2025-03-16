62f341b0d8eb2f2a3745aef33c05f9dafa8bf4ec
Anailson634 <anonymoushack838@gmail.com>
2025-03-16 13:45:55 -0300

    Muando a rota

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 app = Flask(__name__)
-dados_dic={}
 CORS(app)
-
 
 def carregar_pizzas(url_path):
     with open(url_path,  "r") as arq:
         return json.load(arq)
-
-_pizzas=carregar_pizzas("pizzas.json")
-
-
+dados_dic=carregar_pizzas("pizzas.json")
+_pedidos_pizzas={}
+ 
 @app.route("/", methods=["GET", "PUT"])
 def get_dados():
     if request.method=="PUT":
@@ -23,16 +20,16 @@ def get_dados():
             dados_dic[key]=value
     return dados_dic
 
-@app.route("/pizzas", methods=["GET", "PUT"])
+@app.route("/pedidos", methods=["GET", "PUT"])
 def get_pizzas():
     if request.method=="PUT":
         date=request.get_json()
 
         for key, value in date.items():
-            _pizzas[key]=value
-    return _pizzas
+            _pedidos_pizzas[key]=value
+    return _pedidos_pizzas
 
-@app.route("/receber", methods=["POST"])
+@app.route("/login", methods=["POST"])
 def response_front():
     response_date=request.json
     print(response_date)
